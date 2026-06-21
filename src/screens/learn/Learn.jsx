4df066d9373eab8af13187fc769c668266493ffb
Farooq Ahmed <mtf25004128@ue.edu.pk>
2026-06-21 04:57:48 -0700

    perf: using flex and flex direction

---
+++ src/screens/learn/Learn.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import Styles from "./Learn.styles"
+import React from 'react'
+
+export default function Learn() {
+  return (
+    <View style={Styles.container}>
+        <View style={Styles.Box}></View>
+        <View style={Styles.Box2}></View>
+        <View style={Styles.Box3}></View>
+    </View>
+  )
+}
---
+++ src/screens/learn/Learn.styles.jsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+const Styles = StyleSheet.create({
+
+    container:{
+        backgroundColor: 'red',
+        flex:1,
+        flexDirection: 'row',
+    },
+    Box: {
+        backgroundColor: 'blue',
+        flex:0.5,
+    },
+    Box2: {
+        backgroundColor: 'green',
+        flex:0.3,
+    },
+    Box3: {
+        backgroundColor: 'yellow',  
+        flex: 0.2,
+    },
+})
+export default Styles
